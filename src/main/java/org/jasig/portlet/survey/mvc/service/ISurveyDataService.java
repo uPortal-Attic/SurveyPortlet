e044350ae94db177a0f1715fd71f584f2b9ae44c
chasegawa <chasegawa@unicon.net>
2015-03-25 14:58:17 -0700

    SSP-316 Added canonical name to survey and methods to search/retrieve a survey by the name in the REST services

--- src/main/java/org/jasig/portlet/survey/mvc/service/ISurveyDataService.java
+++ src/main/java/org/jasig/portlet/survey/mvc/service/ISurveyDataService.java
@@ -1,4 +1,21 @@
-
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jasig.portlet.survey.mvc.service;
 
 import java.util.List;
@@ -8,17 +25,17 @@ import org.jasig.portlet.survey.service.dto.SurveyDTO;
 import org.jasig.portlet.survey.service.dto.SurveyQuestionDTO;
 
 public interface ISurveyDataService {
-    public SurveyDTO getSurvey( long id);
+    public SurveyDTO addQuestionToSurvey(Long surveyId, Long questionId);
+
+    public AnswerDTO createAnswerForQuestion(Long questionId, AnswerDTO answer);
+    public SurveyDTO createSurvey(SurveyDTO survey);
+    public QuestionDTO createQuestion(QuestionDTO question);
+    public QuestionDTO createQuestionForSurvey(Long surveyId, QuestionDTO question);
+
     public List<SurveyDTO> getAllSurveys();
-    public List<SurveyQuestionDTO> getSurveyQuestions( Long surveyId);
-    
-    public SurveyDTO createSurvey( SurveyDTO survey);
-    public QuestionDTO createQuestion( QuestionDTO question);
-    public QuestionDTO createQuestionForSurvey( Long surveyId, QuestionDTO question);
-    public AnswerDTO createAnswerForQuestion( Long questionId, AnswerDTO answer);
-    
-    public SurveyDTO addQuestionToSurvey( Long surveyId, Long questionId);
-    
-    public SurveyDTO updateSurvey( SurveyDTO survey);
-    public QuestionDTO updateQuestion( QuestionDTO question);
+    public SurveyDTO getSurvey(long id);
+    public List<SurveyQuestionDTO> getSurveyQuestions(Long surveyId);
+
+    public SurveyDTO updateSurvey(SurveyDTO survey);
+    public QuestionDTO updateQuestion(QuestionDTO question);
 }
--- src/main/java/org/jasig/portlet/survey/service/jpa/SurveyDataService.java
+++ src/main/java/org/jasig/portlet/survey/service/jpa/SurveyDataService.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jasig.portlet.survey.service.jpa;
 
 import com.google.common.collect.Lists;
@@ -134,22 +152,23 @@ public class SurveyDataService implements ISurveyDataService {
 
     /**
      * Update question details including embedded answer data
+     * 
      * @param question
      * @return {@link QuestionDTO} or null on error
      */
     @Transactional
     @Override
     public QuestionDTO updateQuestion(QuestionDTO question) {
-        JpaQuestion existingQuestion = surveyDao.getQuestion( question.getId());
-        if( existingQuestion.getStatus() == SurveyState.PUBLISHED) {
+        JpaQuestion existingQuestion = surveyDao.getQuestion(question.getId());
+        if (existingQuestion.getStatus() == SurveyState.PUBLISHED) {
             log.warn( "Cannot update question in PUBLISHED state");
             return null;
         }
-        
-        JpaQuestion jpaQuestion = surveyMapper.toJpaQuestion( question);
-        surveyDao.updateQuestion( jpaQuestion);
-        
-        return surveyMapper.toQuestion( jpaQuestion);
+
+        JpaQuestion jpaQuestion = surveyMapper.toJpaQuestion(question);
+        surveyDao.updateQuestion(jpaQuestion);
+
+        return surveyMapper.toQuestion(jpaQuestion);
     }
 
     /**
