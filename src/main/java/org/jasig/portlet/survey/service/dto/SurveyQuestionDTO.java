440cd7f4dcd34c0c11ffe2497394e78d38ba067b
chasegawa <chasegawa@unicon.net>
2015-03-26 13:12:41 -0700

    SSP-329 Updated the DTO to support added fields

--- src/main/java/org/jasig/portlet/survey/service/dto/SurveyQuestionDTO.java
+++ src/main/java/org/jasig/portlet/survey/service/dto/SurveyQuestionDTO.java
@@ -1,36 +1,54 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jasig.portlet.survey.service.dto;
 
 import java.io.Serializable;
 
 public class SurveyQuestionDTO implements Serializable {
-    private static final long serialVersionUid = 1l;
+    private static final long serialVersionUID = 1l;
 
     private int numAllowedAnswers;
-    private int sequence;
     private QuestionDTO question;
-    
+    private int sequence;
+
     public int getNumAllowedAnswers() {
         return numAllowedAnswers;
-    }
-
-    public void setNumAllowedAnswers(int numAllowedAnswers) {
-        this.numAllowedAnswers = numAllowedAnswers;
-    }
-
-    public int getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(int sequence) {
-        this.sequence = sequence;
     }
 
     public QuestionDTO getQuestion() {
         return question;
     }
 
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setNumAllowedAnswers(int numAllowedAnswers) {
+        this.numAllowedAnswers = numAllowedAnswers;
+    }
+
     public void setQuestion(QuestionDTO question) {
         this.question = question;
     }
-   
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
 }
