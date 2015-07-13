7b8bdd16d4d5cdf8086e2c374cb375a38026d279
Benito J. Gonzalez <bgonzalez@unicon.net>
2015-07-13 14:13:52 -0700

    More testing on summarySpec.js. Looks like a bug in Jasmine Maven module. Issue reported that boot.js is not the right version.

--- src/test/webapp/js/summarySpec.js
+++ src/test/webapp/js/summarySpec.js
@@ -1,11 +1,20 @@
-describe('Controller: summaryController', function() {
+//window.up.startSurveySummaryApp(window, {n: 'test', surveyName: 'surTest'});
+angular.module('test-survey-summary', [])
+    .value('surveyName', 'surTest');
 
-    beforeEach(module('SurveySummaryApp'));
+describe('SurveySummaryApp', function() {
+
+    beforeEach(module('test-survey-summary'));
+
+/*
+    it('survey name should be provided', inject(function(surveyName) {
+        expect(surveyName).toEqual('surTest');
+    }));
 
     var summaryController;
 
     beforeEach(inject(function(_summaryController_) {
         summaryController = _summaryController_;
-    }));
+    }));*/
 
 })
